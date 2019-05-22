749ef062e148084e75959cb8f426aa7a27aefd18
Brooks Burwell <brooksdburwell@gmail.com>
2019-05-21 18:17:49 -0700

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCSlvVhz91V9ZGFdjw51iDLUqMoD2zceLA",
+    authDomain: "trainschedule-e976a.firebaseapp.com",
+    databaseURL: "https://trainschedule-e976a.firebaseio.com",
+    projectId: "trainschedule-e976a",
+    storageBucket: "trainschedule-e976a.appspot.com",
+    messagingSenderId: "869278804424",
+    appId: "1:869278804424:web:5205da749afcc21d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
